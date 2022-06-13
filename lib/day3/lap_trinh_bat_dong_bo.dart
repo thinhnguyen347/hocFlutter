bdfe407846f1baa32624484a27795cb23ee902c0
Thịnh Nguyễn <thinh.ohg@gmail.com>
2022-06-13 18:41:57 +0700

    update bai tap

--- lib/day3/lap_trinh_bat_dong_bo.dart
+++ lib/day3/lap_trinh_bat_dong_bo.dart
@@ -1,23 +1,35 @@
 // ignore_for_file: avoid_print
 
 main() {
+  gioiThieuQueQuan();
   timSoChiaHetCho3();
 }
 
 //1. Tạo ra một chương trình giới thiệu bản thân ấp úng , tức sau 3s mới hiển thị ra
 //Tên, sau 5s mới nói ra tuổi rồi 2s sau mới nói ra quê quán.
 
-// Future<String> gioiThieuTen() async {
-//   Future.delayed(const Duration(seconds: 3), () => 'Toi ten la Thinh');
-// } 
+Future<String> gioiThieuTen() => Future.delayed(
+      const Duration(seconds: 3),
+      () => 'Toi ten la Thinh',
+    );
 
-// String gioiThieuTuoi() {
-//   return 'Toi ten la Thinh';
-// }
+Future<String> gioiThieuTuoi() async {
+  var ten = await gioiThieuTen();
+  print(ten);
+  return Future.delayed(
+    const Duration(seconds: 5),
+    () => 'Toi 31 tuoi',
+  );
+}
 
-// Future<void> gioiThieuQueQuan() async {
-//   return 'Que toi o Ha Noi';
-// }
+Future<void> gioiThieuQueQuan() async {
+  var tuoi = await gioiThieuTuoi();
+  print(tuoi);
+  return Future.delayed(
+    const Duration(seconds: 5),
+    () => print('Toi o Ha Noi'),
+  );
+}
 
 timSoChiaHetCho3() async {
   Duration delayTime = const Duration(seconds: 3);
